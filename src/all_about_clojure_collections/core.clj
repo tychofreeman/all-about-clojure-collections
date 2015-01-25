25a7111c4e1608b6726f1ea5cb9620d9b42826b2
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 09:01:10 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -3,13 +3,3 @@
 ;; This is a list
 '(1 2 3)
 
-;; You can access elements in the list
-(nth '(1 2 3) 2)
-
-;; and in a vector
-(nth [1 2 3] 2)
-(get [1 2 3] 2)
-([1 2 3] 2)
-
-;; By the way, there's also nthnext
-(nthnext '(1 2 3 4 5 6 7 8 9) 4)
