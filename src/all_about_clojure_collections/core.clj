e9f4770973ff1b1f46a05826db74bb20ead21c88
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 09:18:51 -0600

    You may want to use a sequence in its entirety

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -3,3 +3,7 @@
 ;; This is a list
 '(1 2 3)
 
+;; Often, you'll want to consume a sequence in it's entirety
+(map    inc   '(1 2 3 4 5 6))
+(map    first '([1 2] [2 3] [3 4] [4 5] [5 6]))
+(filter even? '(1 2 3 4 5 6))
