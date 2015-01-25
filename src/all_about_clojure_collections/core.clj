9e69ef3521e3f8e6aadd90a0fee250fb547147e6
Christopher Freeman <cwfreeman@gmail.com>
2015-01-24 23:59:11 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -3,20 +3,3 @@
 ;; This is a list
 '(1 2 3)
 
-;; And this is a map
-{"hi" "there"}
-
-;; A map is a collection
-(assert (coll? {"hi" "there"}))
-
-;; but not a seq
-(assert (seq? {"hi" "there"}))
-
-;; And this is a set
-#{1 2 3}
-
-;; A set is a collection
-(assert (coll? #{1 2 3}))
-
-;; but not a seq
-(assert (seq? #{1 2 3}))
