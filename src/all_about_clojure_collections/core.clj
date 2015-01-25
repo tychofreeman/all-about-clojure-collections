a034d0bb620620ec7236f9e3cecf91e9cfed6051
Christopher Freeman <cwfreeman@gmail.com>
2015-01-24 23:48:32 -0600

    So, this is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -2,16 +2,3 @@
 
 ;; This is a list
 '(1 2 3)
-
-;; This is a vector
-[1 2 3]
-
-;; This is the same vector
-(vector 1 2 3)
-
-;; Don't worry; there's nothing confusing here...
-(assert (= '(1 2 3) [1 2 3]))
-
-;; Maybe because it's so easy to interchange them
-(vec '(1 2 3))
-(seq [1 2 3])
