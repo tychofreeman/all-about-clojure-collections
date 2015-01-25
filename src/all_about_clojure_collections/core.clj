ba256975f819aca16f764afe3fa87701d7da982e
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 00:10:06 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -2,13 +2,3 @@
 
 ;; This is a list
 '(1 2 3)
-
-;; You can add stuff to a list
-(conj '(1 2 3) 4)
-
-;; And you can add stuff to a vector
-(conj [1 2 3] 4)
-
-;; Fortunately, there are other ways
-(cons 0 '(1 2 3))
-(cons 0 [1 2 3]) ;; Notice the type
