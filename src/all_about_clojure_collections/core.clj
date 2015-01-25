c9cc63d408b26c8fb5124246ba341bda91f9a616
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 00:05:38 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -3,18 +3,3 @@
 ;; This is a list
 '(1 2 3)
 
-;; For any sequence, you can get first
-(first '(1 2 3))
-
-;; and you can get the rest
-(rest '(1 2 3))
-
-;; And for any collection, you can know if it's empty
-(empty? '())
-(empty? '(1 2 3))
-
-;; By the way, rest has a dual
-(butlast '(1 2 3))
-
-;; And so does first
-(last '(1 2 3))
