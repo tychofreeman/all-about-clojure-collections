ace864371ad0c049e58b2e64259280c6cfd467ca
Christopher Freeman <cwfreeman@gmail.com>
2015-01-24 23:54:44 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -2,15 +2,3 @@
 
 ;; This is a list
 '(1 2 3)
-
-;; And it's a collection
-(assert (coll? '(1 2 3)))
-
-;; And it's a sequence
-(assert (seq? '(1 2 3)))
-
-;; Vectors are collections
-(assert (coll? [1 2 3]))
-
-;; but they are NOT seq's
-(assert (seq? [1 2 3]))
