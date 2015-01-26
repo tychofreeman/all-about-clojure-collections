2e193ad430c8c1d870ce4f2e5432691b48468d95
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 21:30:04 -0600

    Sometimes you want a pipeline for your data

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -4,3 +4,11 @@
 ;; This is a list
 '(1 2 3)
 
+;; Sometimes, you want to create a pipeline
+(->> [1 2 3 4 5 6 7 8]
+     (map #(list % (inc %)))
+     (filter (comp odd? first))
+     flatten
+     reverse
+     sort
+     (map inc))
