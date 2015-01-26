b23fdaebc3b2fae8d25f03cae02e9706a6caaada
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 22:49:13 -0600

    Use 'iterate' to create an infinite, lazy feedback loop

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -6,3 +6,9 @@
 
 ;; repeatedly creates an infinite lazy sequence
 (nth (repeatedly #(rand-int 1000)) 1000)
+
+;; Simple Fibonacci generator
+(take 89 (map second
+              (iterate
+               (fn [[a b]] (list b (+ a b)))
+               '(0 1))))
