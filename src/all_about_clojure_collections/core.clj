0a0477782b3ccf2785ebf2c908c54e10c21e076a
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 20:06:17 -0600

    Mapping is important

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -8,11 +8,4 @@
 (map    first '([1 2] [2 3] [3 4] [4 5] [5 6]))
 (filter even? '(1 2 3 4 5 6))
 
-;; Or your may want to transform it into another type
-(into [] '(1 2 3 4 5))
-(into {} '([:one 1] [:two 2] [:three 3]))
-(into [1 2 3] '(1 2 3 4 5 6))
 
-;; Or you may want to do something arbitrary
-(reduce + [1 2 3 4])
-(reduce conj [] '(1 2 3 4 5))
