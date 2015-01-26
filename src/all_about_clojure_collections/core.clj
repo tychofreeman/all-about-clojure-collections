577e12e4682cfeb0aeb503989d5e691654fd1756
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 21:32:58 -0600

    That's all for lists

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -1,12 +1,3 @@
 (ns all-about-clojure-collections.core
   (:require [clojure.string :as str]))
 
-;; This is a list
-'(1 2 3)
-
-;; The function seq is interesting
-(seq nil)
-(seq [])
-(seq [1])
-(seq {1 1 2 2 3 3})
-
