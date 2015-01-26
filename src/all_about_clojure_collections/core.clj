454f6444490f1e3602cf57820c43f96041f21637
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 21:38:36 -0600

    This is a map

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -4,11 +4,3 @@
 ;; This is a map
 {:a 1 :b 2 :c 3 :d 4}
 
-;; You can get values from a map
-(get {:a 1} :a)
-
-;; A map will act like a function
-({:a 1} :a)
-
-;; A keyword will also act like a function
-(:a {:a 1})
