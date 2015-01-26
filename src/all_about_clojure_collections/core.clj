2f28ec60ae82ed278bd3b20127305998fa3f65f4
Christopher Freeman <cwfreeman@gmail.com>
2015-01-25 21:31:07 -0600

    This is a list

--- src/all_about_clojure_collections/core.clj
+++ src/all_about_clojure_collections/core.clj
@@ -4,11 +4,3 @@
 ;; This is a list
 '(1 2 3)
 
-;; Sometimes, you want to create a pipeline
-(->> [1 2 3 4 5 6 7 8]
-     (map #(list % (inc %)))
-     (filter (comp odd? first))
-     flatten
-     reverse
-     sort
-     (map inc))
